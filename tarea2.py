68a609fafd8fc6e44d94b58163e07f095a03aff6
Ana Sofia <ana.suarez.febres@gmail.com>
2022-11-02 22:40:38 -0500

    todas las generaciones

--- tarea2.py
+++ tarea2.py
@@ -26,37 +26,149 @@ data = response.json()
 #Del 1 al 151 son gen 1
 
 #Generación 1
-url_api = 'https://pokeapi.co/api/v2/pokemon/'
-params = {'limit': 151}
+def gen_uno():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 151}
 
-for offfset in range(0, 152, 200):
-    params['offset'] = offfset
-    response = requests.get(url_api, params=params)
+    for offfset in range(0, 152, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
 
-    if response.status_code != 200:
-        print(response.text)
-    else:
-        data = response.json()
-        for item in data['results']:
-            print(item['name'])
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
 
+#Generación 2
+def gen_dos():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 100}
 
+    for offfset in range(151, 251, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
 
-# def get_pokemon(gen):
-#     if gen == 'gen 1': 
-#         for number in range(0, 152):
-#             str(number)
-#         com_url = [number for number in range(0, 152)]
-#         for number in com_url:
-#             response_api = requests.get(url_api + str(number))
-#     data = response_api.json()
-    
-    
-#     lista_nombres = {data['names'][0]['name']} #Solo me sale el 151 :(
-    
-#     print(f'Nombres:{lista_nombres}')
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
 
-# get_pokemon('gen 1')
+#Generación 3
+def gen_tres():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 135}
 
+    for offfset in range(251, 386, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
 
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
 
+#Generación 4
+def gen_cuatro():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 107}
+
+    for offfset in range(386, 493, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
+
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
+
+#Generación 5
+def gen_cinco():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 156}
+
+    for offfset in range(493, 649, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
+
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
+
+#Generación 6
+def gen_seis():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 72}
+
+    for offfset in range(649, 721, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
+
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
+
+#Generación 7
+def gen_siete():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 88}
+
+    for offfset in range(721, 809, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
+
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
+
+#Generación 8
+def gen_ocho():
+    url_api = 'https://pokeapi.co/api/v2/pokemon/'
+    params = {'limit': 96}
+
+    for offfset in range(809, 905, 200):
+        params['offset'] = offfset
+        response = requests.get(url_api, params=params)
+
+        if response.status_code != 200:
+            print(response.text)
+        else:
+            data = response.json()
+            for item in data['results']:
+                print(item['name'])
+
+opcion_1 = input('Ingresa el número de la generación a listar: ')
+if int(opcion_1) == 1:
+    gen_uno()
+elif int(opcion_1) ==2:
+    gen_dos()
+elif int(opcion_1) == 3:
+    gen_tres()
+elif int(opcion_1) == 4:
+    gen_cuatro()
+elif int(opcion_1) == 5:
+    gen_cinco()
+elif int(opcion_1) == 6:
+    gen_seis()
+elif int(opcion_1) == 7:
+    gen_siete()
+elif int(opcion_1) == 8:
+    gen_ocho()
+else:
+    print('Opción incorrecta.')
