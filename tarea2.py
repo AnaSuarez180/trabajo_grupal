664c01b74de7cdbbf1af9c3090a22c692a664fec
Ana Sofia <ana.suarez.febres@gmail.com>
2022-11-02 21:58:06 -0500

    actualizada gen 1

--- tarea2.py
+++ tarea2.py
@@ -25,20 +25,38 @@ data = response.json()
 #Quizás puedo aumentarle números al final del url hasta alcanzar el límite de la gen??
 #Del 1 al 151 son gen 1
 
-#Listar por generación
-def get_pokemon(gen):
-    if gen == 'gen 1': 
-        for number in range(0, 152):
-            str(number)
-    response_api = requests.get(url_api + str(number))
-    data = response_api.json()
-    
-    
-    lista_nombres = {data['name']} #Solo me sale el 151 :(
-    
-    print(f'Nombres:{lista_nombres}')
+#Generación 1
+url_api = 'https://pokeapi.co/api/v2/pokemon/'
+params = {'limit': 151}
 
-get_pokemon('gen 1')
+for offfset in range(0, 152, 200):
+    params['offset'] = offfset
+    response = requests.get(url_api, params=params)
+
+    if response.status_code != 200:
+        print(response.text)
+    else:
+        data = response.json()
+        for item in data['results']:
+            print(item['name'])
+
+
+
+# def get_pokemon(gen):
+#     if gen == 'gen 1': 
+#         for number in range(0, 152):
+#             str(number)
+#         com_url = [number for number in range(0, 152)]
+#         for number in com_url:
+#             response_api = requests.get(url_api + str(number))
+#     data = response_api.json()
+    
+    
+#     lista_nombres = {data['names'][0]['name']} #Solo me sale el 151 :(
+    
+#     print(f'Nombres:{lista_nombres}')
+
+# get_pokemon('gen 1')
 
 
 
