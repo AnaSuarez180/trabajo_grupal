02acfdbef72c1122e65aa99464788e4fde611154
Ana Sofia <ana.suarez.febres@gmail.com>
2022-11-04 20:56:47 -0500

    funciones 2, 3, 4, 5, 6

--- tarea1.py
+++ tarea1.py
@@ -23,32 +23,70 @@ y autores).
 Opción 10: Guardar libros en archivo de disco duro (.txt o csv).
 Nota: listar libros involucra: título, género, ISBN, editorial y autor(es)"""
 
-import csv
+
 import pandas as pd
-import json
-
-
-#data = list(csv.reader(open(input('Escriba la dirección del archivo a leer: '))))
-
-
-
-
+from csv import writer
+import csv
 
 #data = list(csv.reader(open(input('Escriba la dirección del archivo a leer: '))))
 
-
-data = pd.read_csv(r'C:\Users\sofia\Desktop\SILABUZ CLASES\SEMANA 2\TRABAJO GRUPAL\books2.csv')
+data = pd.read_csv('books2.csv', sep=";") #index_col=0,
 
 def listar_libros():
-    for head in data:
-        print(data)
+    print(data)
 
-listar_libros()
+# listar_libros()
+
+#AGREGAR LIBRO
+
+# datos = [input('Id: '), input('Título: '), input('Género: '), input('ISBN: '), input('Editorial: '), input('Autores: ')]
+def append_list_as_row(file_name, raw_content):
+    
+    with open(file_name,'a+',newline='') as write_obj:
+        cvs_writer = writer(write_obj)
+        cvs_writer.writerow(raw_content)
+        print('¡Listo! Revisa en la ruta que indica la terminal.')
+
+# append_list_as_row('books2.csv', datos)
 
 
+def eliminar_libro(file_name):
+    eliminado = input('Ingresa el libro a eliminar: ')
+    with open(file_name, 'r+') as r, open('output.csv', 'w') as f:
+        reader = csv.reader(r)
+        writer = csv.writer(f)
+        for row in reader:
+            if eliminado in row:
+                print(f'{row} será eliminada')
+            else:
+                writer.writerow(row)
+
+# eliminar_libro('books2.csv')
 
 
+def buscar_libro():
+    df = pd.DataFrame(data)
+    eleccion = input('Título o ISBN: ')
+    if eleccion.lower() == 'título':
+        libro = input('Ingresa el libro: ')
+        df = df[df.apply(lambda r: r.str.contains(libro, na=False).any(), axis=1)]
+        print(df)
+    elif eleccion.lower() == 'isbn':
+        isbn = input('Ingresa el ISBN: ')
+        df = df[df.apply(lambda r: r.str.contains(isbn, na=False).any(), axis=1)]
+        print(df)
+    else:
+        print('Opción incorrecta.')
 
+# buscar_libro()
+
+#ORDERNAR LIBROS POR TÍTULO
+def ordenar_libros():
+    csvData = pd.read_csv('books2.csv')
+    csvData.sort_values(csvData.columns[1], axis=0, inplace=True)
+    print(csvData)
+
+ordenar_libros() #El primero es Angels and Demons, el último es Twilight
 
 
 
